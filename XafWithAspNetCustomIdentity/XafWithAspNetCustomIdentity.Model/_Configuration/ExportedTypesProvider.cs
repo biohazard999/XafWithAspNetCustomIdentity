16300f9f243592dee86c5c1398e0dd93927be376
Manuel Grundner <m.grundner@delegate.at>
2014-03-06 20:47:57 +0100

    Added all types needed to run the demo with asp.net

---
+++ XafWithAspNetCustomIdentity/XafWithAspNetCustomIdentity.Model/_Configuration/ExportedTypesProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XafWithAspNetCustomIdentity.Model._Configuration
+{
+    public static class ExportedTypesProvider
+    {
+        public static IEnumerable<Type> ExportedTypes
+        {
+            get
+            {
+                return new[]
+                {
+                    typeof(AspClaim),
+                    typeof(AspRole),
+                    typeof(AspUser),
+                    typeof(AspUserLogin),
+                };
+            }
+        }
+    }
+}
--- XafWithAspNetCustomIdentity/XafWithAspNetCustomIdentity.Module/Module.cs
+++ XafWithAspNetCustomIdentity/XafWithAspNetCustomIdentity.Module/Module.cs
@@ -1,18 +1,9 @@
 using System;
-using System.Text;
 using System.Linq;
 using DevExpress.ExpressApp;
-using System.ComponentModel;
-using DevExpress.ExpressApp.DC;
 using System.Collections.Generic;
-using DevExpress.Persistent.Base;
-using DevExpress.ExpressApp.Model;
-using DevExpress.ExpressApp.Actions;
-using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Updating;
-using DevExpress.ExpressApp.Model.Core;
-using DevExpress.ExpressApp.Model.DomainLogics;
-using DevExpress.ExpressApp.Model.NodeGenerators;
+using XafWithAspNetCustomIdentity.Model._Configuration;
 
 namespace XafWithAspNetCustomIdentity.Module
 {
@@ -23,6 +14,12 @@ namespace XafWithAspNetCustomIdentity.Module
         {
             InitializeComponent();
         }
+
+        protected override IEnumerable<Type> GetDeclaredExportedTypes()
+        {
+            return base.GetDeclaredExportedTypes().Concat(ExportedTypesProvider.ExportedTypes);
+        }
+
         public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB)
         {
             ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
