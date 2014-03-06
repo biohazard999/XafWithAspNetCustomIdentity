391482fbd172d108c5afd3be33213abd2304b879
Manuel Grundner <m.grundner@delegate.at>
2014-03-06 20:53:47 +0100

    added ASP.NET Project

---
+++ XafWithAspNetCustomIdentity/XafWithAspNetCustomIdentity.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XafWithAspNetCustomIdentity.Web.Startup))]
+namespace XafWithAspNetCustomIdentity.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
